15e9066836a7e2ed1c6f2e98e891d6fee60483af
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-06-01 12:52:40 +0530

    Time: 5 ms (96.09%) | Memory: 44.6 MB (51.02%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int n = cost.length;
+        if (n<2) return cost[n-1];
+        if(n<3) return cost[0]+cost[1];
+        int sum=0;
+        int i=n-2,j=n-1;
+        while(i>=0 && j>=0){
+            
+            sum+=cost[i]+cost[j];
+            i-=3;
+            j-=3;
+        }
+        if(i<0 && j>=0){
+                sum+=cost[j];
+            }
+        return sum;
+    }
+}
