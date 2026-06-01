6916be5de102b6c5a90cfb9be553a0cdf70070af
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-06-01 12:58:16 +0530

    Time: 5 ms (96.09%) | Memory: 44.6 MB (51.02%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -2,19 +2,26 @@ class Solution {
     public int minimumCost(int[] cost) {
         Arrays.sort(cost);
         int n = cost.length;
-        if (n<2) return cost[n-1];
-        if(n<3) return cost[0]+cost[1];
+        // if (n<2) return cost[n-1];
+        // if(n<3) return cost[0]+cost[1];
         int sum=0;
-        int i=n-2,j=n-1;
-        while(i>=0 && j>=0){
+        // int i=n-2,j=n-1;
+        // while(i>=0 && j>=0){
             
-            sum+=cost[i]+cost[j];
-            i-=3;
-            j-=3;
+        //     sum+=cost[i]+cost[j];
+        //     i-=3;
+        //     j-=3;
+        // }
+        // if(i<0 && j>=0){
+        //         sum+=cost[j];
+        //     }
+        // return sum;
+        int a=0;
+        for(int i=n-1;i>=0;i--){
+            a++;
+            if(a==3){a=0;continue;}
+            sum+=cost[i];
         }
-        if(i<0 && j>=0){
-                sum+=cost[j];
-            }
         return sum;
     }
 }
