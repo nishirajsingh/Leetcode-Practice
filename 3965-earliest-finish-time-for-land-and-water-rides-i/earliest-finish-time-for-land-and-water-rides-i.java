4f571d82dc5d8211a466b98e5680825c0279fa19
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-06-02 17:23:28 +0530

    Time: 1 ms (100.00%) | Memory: 46.9 MB (39.29%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,22 +1,20 @@
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int a = Integer.MAX_VALUE;
-        for (int i = 0; i < landStartTime.length; i++) {
-            a = Math.min(a, landStartTime[i] + landDuration[i]);
+        int a=1000000;
+        for (int i=0;i<landStartTime.length;i++) {
+            a=Math.min(a,landStartTime[i]+landDuration[i]);
         }
-        int b = Integer.MAX_VALUE;
-        for (int i = 0; i < waterStartTime.length; i++) {
-            b = Math.min(b, waterStartTime[i] + waterDuration[i]);
+        int b=1000000;
+        for (int i=0;i<waterStartTime.length;i++) {
+            b=Math.min(b,waterStartTime[i]+waterDuration[i]);
         }
-        int sum = Integer.MAX_VALUE;
-        for (int i = 0; i < waterStartTime.length; i++) {
-            int c = Math.max(a, waterStartTime[i]);
-            sum = Math.min(sum, c + waterDuration[i]);
+        int total=1000000;
+        for (int i=0;i<waterStartTime.length;i++) {
+            total=Math.min(total, Math.max(a,waterStartTime[i])+waterDuration[i]);
         }
-        for (int i = 0; i < landStartTime.length; i++) {
-            int c = Math.max(b, landStartTime[i]);
-            sum = Math.min(sum, c + landDuration[i]);
+        for (int i=0;i<landStartTime.length;i++) {
+            total=Math.min(total, Math.max(b,landStartTime[i])+landDuration[i]);
         }
-        return sum;
+        return total;
     }
 }
