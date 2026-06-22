4692d700564bc386c0934e2529c4178077b4a2ea
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-06-22 08:23:15 +0530

    Time: 3 ms (70.35%) | Memory: 43.3 MB (71.44%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -3,13 +3,7 @@ class Solution {
         int[] cnt = new int[26];
         for(int i=0;i<text.length();i++){
             cnt[text.charAt(i)-'a']++;
-        }
-        int b = cnt['b' - 'a'];
-        int a = cnt['a' - 'a'];
-        int l = cnt['l' - 'a']/2;
-        int o = cnt['o' - 'a']/2;
-        int n = cnt['n' - 'a'];
-        
-        return Math.min(b,Math.min(a,Math.min(l,Math.min(o,n))));
+        }        
+        return Math.min(cnt['b'-'a'],Math.min(cnt['a'-'a'],Math.min(cnt['l'-'a']/2,Math.min(cnt['o'-'a']/2,cnt['n'-'a']))));
     }
 }
