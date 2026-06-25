ecbab3a8d14aed7e1c450858d3f4eb4efb249dbf
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-06-25 10:28:43 +0530

    Time: 1 ms (98.49%) | Memory: 47.1 MB (98.36%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -4,37 +4,36 @@ class Solution {
         int m = matrix[0].length;
         int fc=0;
         int fr=0;
-
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
                     if(i==0){
-                        fc=1;
+                        fr=1;
                     }
                     if(j==0){
-                        fr=1;
+                        fc=1;
                     }
                     matrix[i][0]=0;
                     matrix[0][j]=0;
+
                 }
             }
         }
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
-                if(matrix[i][0]==0||matrix[0][j]==0){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
-
-        if(fr==1){
-        for(int i=0;i<n;i++){
-            matrix[i][0]=0;
-        }
-        }
         if(fc==1){
-            for(int i=0;i<m;i++){
-                matrix[0][i]=0;
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(fr==1){
+            for(int j=0;j<m;j++){
+                matrix[0][j]=0;
             }
         }
         
