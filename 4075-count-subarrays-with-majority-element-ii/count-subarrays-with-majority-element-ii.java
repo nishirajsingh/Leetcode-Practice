680ef32b3d33451f9ce56797fbf7933a02b735ac
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-06-26 15:55:28 +0530

    Time: 14 ms (67.21%) | Memory: 87.9 MB (81.97%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
@@ -1,39 +1,40 @@
-class BIT {
+class ft{
     private int n;
-    private int[] c;
-
-    public BIT(int n) {
+    private int[]c;
+    public ft(int n){
         this.n = n;
-        this.c = new int[n + 1];
+        this.c = new int[n+1];
     }
-
-    public void update(int x, int delta) {
-        for (; x <= n; x += x & -x) {
-            c[x] += delta;
+    public void update(int s,int delta){
+        for(;s<=n;s+=s&(-s)){
+            c[s]+=delta;
         }
     }
-
-    public int query(int x) {
-        int s = 0;
-        for (; x > 0; x -= x & -x) {
-            s += c[x];
+    public int query(int x){
+        int s=0;
+        for(;x>0;x-=x&(-x)){
+            s+=c[x];
         }
         return s;
+
     }
 }
-
 class Solution {
     public long countMajoritySubarrays(int[] nums, int target) {
         int n = nums.length;
-        BIT tree = new BIT(2 * n + 1);
-        int s = n + 1;
-        tree.update(s, 1);
-        long ans = 0;
-        for (int x : nums) {
-            s += x == target ? 1 : -1;
-            ans += tree.query(s - 1);
-            tree.update(s, 1);
+        ft tree = new ft(2*n+1);
+        int s = n+1;
+        tree.update(s,1);
+        long res=0;
+        for(int i: nums){
+            if(i==target){
+                s+=1;
+            }
+            else s-=1;
+            res+=tree.query(s-1);
+            tree.update(s,1);
         }
-        return ans;
+        return res;
+
     }
 }
