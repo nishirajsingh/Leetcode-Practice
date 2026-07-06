ffd7099875abdce40b7f1049799f47994a83715b
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-07-06 11:03:07 +0530

    Time: 4 ms (48.67%) | Memory: 44.7 MB (48.57%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s= s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for (int i =0;i<s.length();i++){
+            char c = s.charAt(i);
+            if((c>='a' && c<='z') || (c>='0' && c<='9')){
+                sb.append(c);
+            }
+        }
+        return ispalindrome(sb.toString());
+    }
+    public boolean ispalindrome(String s){
+        for(int i = 0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(s.length()-i-1))return false;
+        }
+        return true;
+    }
+}
