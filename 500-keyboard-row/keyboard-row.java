306677bb07ab37995bdd0d46e109cbca2f99d0da
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-07-08 13:55:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (32.73%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -1,18 +1,29 @@
 class Solution {
     public String[] findWords(String[] words) {
-        String q="qwertyuiopQWERTYUIOP";
-        String a="asdfghjklASDFGHJKL";
-        String z="zxcvbnmZXCVBNM";
-        List<String> res = new ArrayList<>();
-        for(String s:words){
-            if(isValid(s,q)||isValid(s,a)||isValid(s,z)) res.add(s);
+    int[] map = new int[128];
+    String[] rows = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+    
+    for (int i = 0; i < rows.length; i++) {
+        for (char c : rows[i].toCharArray()) {
+            map[c] = i;
+            map[Character.toUpperCase(c)] = i;
         }
-        return res.toArray(new String[0]);
     }
-    public boolean isValid(String s,String t){
-        for(int i=0;i<s.length();i++){
-            if((t.indexOf(s.charAt(i))==-1)) return false;
-        }
-        return true;
+
+    List<String> res = new ArrayList<>();
+    for (String s : words) {
+        if (s.isEmpty()) continue;
+        int targetRow = map[s.charAt(0)];
+        boolean isValid = true;
+        
+        for (int i = 1; i < s.length(); i++) {
+            if (map[s.charAt(i)] != targetRow) {
+                isValid = false;
+                break;
+            }
+        }   
+        if (isValid) res.add(s);
     }
+    return res.toArray(new String[0]);
+}
 }
