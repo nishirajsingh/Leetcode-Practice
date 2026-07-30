9db8cb25d67c350e7e41d68d71970ec89bf3bb75
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-07-30 10:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (95.69%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int res=0;
+        for(int i=0;i<word.length();i++){
+            res += (i/8)+1;
+        }
+        return res;
+    }
+}
