f9a56d18176f4b3f290c5f78bc0aa4f8f3d5f0c8
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-08-03 20:47:48 +0530

    Time: 42 ms (32.67%) | Memory: 93.7 MB (27.84%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -0,0 +1,32 @@
+class Solution {
+    int[] stoneValue;
+    Integer[] f;
+    int n;
+
+    public String stoneGameIII(int[] stoneValue) {
+        this.stoneValue = stoneValue;
+        this.n = stoneValue.length;
+        this.f = new Integer[n];
+        int res = dfs(0);
+        if (res == 0) {
+            return "Tie";
+        }
+        return res > 0 ? "Alice" : "Bob";
+    }
+
+    public int dfs(int i) {
+        if (i >= n) {
+            return 0;
+        }
+        if (f[i] != null) {
+            return f[i];
+        }
+        int ans = Integer.MIN_VALUE;
+        int s = 0;
+        for (int j = i; j < i + 3 && j < n; j++) {
+            s += stoneValue[j];
+            ans = Math.max(ans, s - dfs(j + 1));
+        }
+        return f[i] = ans;
+    }
+}
