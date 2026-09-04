8f7392576635a6d05e9ee2e69d894a44dc4f43cb
Nishiraj Singh Panwar <nishirajsingh2005@gmail.com>
2026-09-04 23:14:32 +0530

    Time: 2 ms (85.93%) | Memory: 42.1 MB (99.97%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] a = new boolean[s.length()+1];
+        var a = new boolean[s.length()+1];
         a[0]=true;
         for(int i=0;i<s.length();i++){
             if(!a[i])continue;
